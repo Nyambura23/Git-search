5bf4926e1e68038df172767035ab679043b38e52
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-18 02:51:04 +0300

    generate API token and add to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  githubApiToken: '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',
 };
 
 /*
