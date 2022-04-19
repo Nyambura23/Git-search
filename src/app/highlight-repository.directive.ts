e15f65f2aadb2120750aff54e304f66354ea1e24
Nyambura🌻 <bianca@Biancas-MacBook-Air.local>
2022-04-19 10:11:15 +0300

    apply attribute dir ective then handle user events

--- src/app/highlight-repository.directive.ts
+++ src/app/highlight-repository.directive.ts
@@ -1,12 +1,23 @@
-import { Directive,ElementRef } from '@angular/core';
+import { Directive,ElementRef,HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightRepository]'
 })
 export class HighlightRepositoryDirective {
 
-  constructor(private elem:ElementRef) { 
-    
+  constructor(private elem:ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('cyan');
+  }
+  
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight('');
+  }
+
+  private highlight(action:string){
+    this.elem.nativeElement.style.backgroundColor = 'cyan';
+
   }
 
 }
